4c262426c7ee16524d0ce7c05b1eade18191d220
Loren Pabst <lorenpabst@Lorens-MacBook-Pro.local>
2021-01-23 11:46:12 -0700

    canvas methods work how I want

--- canvas.js
+++ canvas.js
@@ -3,10 +3,12 @@ function Canvas(id = "canvas") {
   this.canvas = document.getElementById(id);
   this.context = this.canvas.getContext("2d");
 
+  // draws black over the entire canvas
   this.clear = function () {
-    this.drawRect(0, 0, this.canvas.width, this.canvas.height, "black");
+    this.drawRect(0, 0, this.canvas.width, this.canvas.height, "black", true);
   };
 
+  // top left corner of line goes on x, y
   this.drawLine = function (x, y, toX, toY, color) {
     this.context.beginPath();
     this.context.strokeStyle = color;
@@ -15,6 +17,7 @@ function Canvas(id = "canvas") {
     this.context.stroke();
   };
 
+  // center of circle goes on x, y
   this.drawCircle = function (x, y, radius, color = "white", fill = false) {
     this.context.beginPath();
     if (fill) {
@@ -26,9 +29,9 @@ function Canvas(id = "canvas") {
       this.context.arc(x, y, radius, 0, 2 * Math.PI);
       this.context.stroke();
     }
-    // this.context.endPath();
   };
 
+  // top left corner of rect goes on x, y
   this.drawRect = function (
     x,
     y,
@@ -48,7 +51,10 @@ function Canvas(id = "canvas") {
     }
   };
 
-  this.drawText = function () {
-    //
+  // top left corner of text goes on x, y
+  this.drawText = function (x, y, text, color = "white", fontSize = "14") {
+    this.context.font = fontSize + "px Arial";
+    this.context.fillStyle = color;
+    this.context.fillText(text, x, y + fontSize * 0.8);
   };
 }
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const game = {
-  init: () => {
+  init: function () {
     const canvas = new Canvas("canvas");
     canvas.clear();
-    canvas.drawLine(20, 20, 60, 20, "orange");
-    canvas.drawCircle(100, 100, 50, 50, "blue", false);
-    canvas.drawCircle(200, 100, 50, 50, "blue", true);
-    canvas.drawRect(300, 300, 50, 50, "yellow", true);
-    canvas.drawRect(400, 400, 80, 20, "purple", false);
+    const data = { canvas };
+    game.run(data);
+  },
+
+  run: function (data) {
+    console.log(data);
   },
 };
 
